195f9c5ca06730ce05d28777b86ec7a3e138a2b0
Lia OBrien <liaobrien123@gmail.com>
2021-11-03 20:07:29 -0400

    filled out install event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,19 @@ const FILES_TO_CACHE = [
       './icons/icon-192x192.png',
       './icons/icon-512x512.png'
 ];
+
+// install
+self.addEventListener("install", (evt) => {
+      // pre cache transactions
+      evt.waitUntil(
+            caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transactions"))
+      );
+
+      // pre cache bulk transactions (may only need this or the above?)
+      evt.watiUntil(
+            caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transactions/bulk"))
+      );
+
+      self.skipWaiting();
+});
+
